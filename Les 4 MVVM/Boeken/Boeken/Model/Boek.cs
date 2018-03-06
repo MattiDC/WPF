049a5f928ed34aae0eea764045bcc628b21a7f73
MattiDC <deconinckmatt@gmail.com>
2018-03-06 23:05:35 +0100

    Nog werk aan de layout

---
+++ Les 4 MVVM/Boeken/Boeken/Model/Boek.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boeken.Model
+{
+    class Boek : BaseModel
+    {
+        private string titel;
+        private string auteur;
+        private string samenvatting;
+        private string foto;
+
+        //constructor boek
+        public Boek(string titel, string auteur, string samenvatting, string foto)
+        {
+            Titel = titel;
+            Auteur = auteur;
+            Samenvatting = samenvatting;
+            Foto = foto;
+        }
+
+        //getters en setters
+        public string Titel
+        {
+            get => titel; set
+            {
+                titel = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string Auteur
+        {
+            get => auteur; set
+            {
+                auteur = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string Samenvatting
+        {
+            get => samenvatting; set
+            {
+                samenvatting = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string Foto
+        {
+            get => foto; set
+            {
+                foto = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
---
+++ Les 4 MVVM/Boeken/Boeken/ViewModel/BoekenViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Boeken.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boeken.ViewModel
+{
+    class BoekenViewModel : BaseViewModel
+    {
+        private ObservableCollection<Boek> boeken;
+        public ObservableCollection<Boek> Boeken
+        {
+            get { return boeken; }
+            set
+            {
+                boeken = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public BoekenViewModel()
+        {
+            LadenBoeken();
+        }
+
+        public void LadenBoeken()
+        {
+            Boeken = new ObservableCollection<Boek>();
+
+            //geeft running directory van de running appliction
+            //encoding nodig om onleesbare tekens weg te werken
+            using (StreamReader sr = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "/Data/boeken.txt", Encoding.UTF7))
+            {
+                while (!sr.EndOfStream)
+                {
+                    String line = sr.ReadLine();
+                    //parsen van string naar character
+                    string[] substrings = line.Split(char.Parse(";"));
+
+                    Boeken.Add(new Boek(substrings[1],substrings[0],substrings[3], AppDomain.CurrentDomain.BaseDirectory + substrings[2]));
+                }
+            }
+        }
+    }
+}
