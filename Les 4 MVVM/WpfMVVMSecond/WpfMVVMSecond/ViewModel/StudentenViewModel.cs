43612d8c4b0da54d5024f191e28731cdb492f173
MattiDC <deconinckmatt@gmail.com>
2018-03-06 13:02:17 +0100

    Powerpoint afgewerkt WpfMVVM

--- Les 4 MVVM/WpfMVVMSecond/WpfMVVMSecond/ViewModel/StudentenViewModel.cs
+++ Les 4 MVVM/WpfMVVMSecond/WpfMVVMSecond/ViewModel/StudentenViewModel.cs
@@ -7,11 +7,22 @@ namespace WpfMVVMSecond.ViewModel
     class StudentenViewModel : BaseViewModel
     {
         private int nummer;
- 
+
         public StudentenViewModel()
         {
+            LadenStudenten();
+            KoppelenCommands();
         }
-        
+
+        private void LadenStudenten()
+        {
+            Studenten = new ObservableCollection<Student>();
+            Studenten.Add(new Student("Jef Verboven", "2 ITF", 2));
+            Studenten.Add(new Student("Hans Verkerken", "2 ITF", 1));
+            Studenten.Add(new Student("Lies Pauwels", "2 ITF", 3));
+            Studenten.Add(new Student("Stefanie Moreels", "2 ITF", 2));
+        }
+
         private ObservableCollection<Student> studenten;
         public ObservableCollection<Student> Studenten
         {
@@ -25,7 +36,45 @@ namespace WpfMVVMSecond.ViewModel
                 NotifyPropertyChanged();
             }
         }
-        
+
+        //code ICommands
+        private ICommand addStudentCommand;
+        private ICommand deleteStudentCommand;
+  
+        public ICommand AddStudentCommand { get { return addStudentCommand; } set { addStudentCommand = value; } }
+        public ICommand DeleteStudentCommand { get { return deleteStudentCommand; } set {deleteStudentCommand = value; } }
+
+        //kopelen van commands aan gewenste actie
+        private void KoppelenCommands()
+        {
+            AddStudentCommand = new BaseCommand(Toevoegen);
+            DeleteStudentCommand = new BaseCommand(Verwijderen);
+        }
+
+        private void Toevoegen()
+        {
+            Studenten.Add(new Student("Student " + (++nummer), "2 ITF", nummer));
+        }
+
+        //voordat we kunnen verwijderen moeten we eerst kijken of we een item hebben geselecteerd
+        private Student selectedItem;
+        public Student SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                selectedItem = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void Verwijderen()
+        {
+            if (SelectedItem != null)
+            {
+                Studenten.Remove(SelectedItem);
+            }
+        }
     }
 
 }
