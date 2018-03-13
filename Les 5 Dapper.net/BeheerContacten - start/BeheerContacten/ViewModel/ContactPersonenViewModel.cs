970a2bc50241c93c9ca1c471219e914faf29a2a6
MattiDC <deconinckmatt@gmail.com>
2018-03-13 12:41:09 +0100

    BeheerContacten af vanuit PPT

---
+++ Les 5 Dapper.net/BeheerContacten - start/BeheerContacten/ViewModel/ContactPersonenViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.ObjectModel;
+using BeheerContacten.Model;
+using System.Windows.Input;
+
+namespace BeheerContacten.ViewModel
+{
+    public class ContactPersonenViewModel : BaseViewModel
+    {        
+        public ContactPersonenViewModel()
+        {
+            LeesContactPersonen();
+            KoppelenCommands();
+        }
+
+        private ObservableCollection<ContactPersoon> contactpersonen;
+        public ObservableCollection<ContactPersoon> Contactpersonen
+        {
+            get
+            {
+                return contactpersonen;
+            }
+
+            set
+            {
+                contactpersonen = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private ContactPersoon currentContact;
+        public ContactPersoon CurrentContact
+        {
+            get
+            {
+                return currentContact;
+            }
+
+            set
+            {
+                currentContact = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void KoppelenCommands()
+        {
+            WijzigenCommand = new BaseCommand(WijzigenContactPersoon);
+            VerwijderenCommand = new BaseCommand(VerwijderenContactPersoon);
+            ToevoegenCommand = new BaseCommand(ToevoegenContactPersoon);
+        }
+
+        public ICommand VerwijderenCommand { get; set; }
+        public ICommand WijzigenCommand { get; set; }
+        public ICommand ToevoegenCommand { get; set; }
+
+        private void LeesContactPersonen()
+        {
+            //instantiëren dataservice
+            ContactPersoonDataService contactDS = new ContactPersoonDataService();
+            Contactpersonen = new ObservableCollection<ContactPersoon>(contactDS.GetContactPersonen());
+
+        }
+
+        public void WijzigenContactPersoon()
+        {
+            if (CurrentContact != null)
+            {
+                ContactPersoonDataService contactDS = new ContactPersoonDataService();
+                contactDS.UpdateContactPersoon(CurrentContact);
+
+                //Refresh
+                LeesContactPersonen();
+            }
+        }
+
+        public void ToevoegenContactPersoon()
+        {
+            ContactPersoonDataService contactDS = new ContactPersoonDataService();
+            contactDS.InsertContactPersoon(CurrentContact);
+
+            //Refresh
+            LeesContactPersonen();
+        }
+
+
+        public void VerwijderenContactPersoon()
+        {
+            if (CurrentContact != null)
+            {
+                ContactPersoonDataService contactDS =
+                    new ContactPersoonDataService();
+                contactDS.DeleteContactPersoon(CurrentContact);
+
+                //Refresh
+                LeesContactPersonen();
+            }
+        }
+
+    }
+
+}
