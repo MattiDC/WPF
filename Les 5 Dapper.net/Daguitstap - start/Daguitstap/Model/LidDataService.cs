117d7548d04d3881ef10e1c5876ce2738eebcce3
MattiDC <deconinckmatt@gmail.com>
2018-03-17 11:40:23 +0100

    Zonder uitbreiding

---
+++ Les 5 Dapper.net/Daguitstap - start/Daguitstap/Model/LidDataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Daguitstap.Model
+{
+    class LidDataService
+    {
+        // Ophalen ConnectionString uit App.config
+        private static string connectionString =
+            ConfigurationManager.ConnectionStrings["azure"].ConnectionString;
+
+        // Stap 1 Dapper
+        // Aanmaken van een object uit de IDbConnection class en 
+        // instantiëren van een SqlConnection.
+        // Dit betekent dat de connectie met de database automatisch geopend wordt.
+        private static IDbConnection db = new SqlConnection(connectionString);
+
+        public List<Lid> GetDeelnemers(bool neemtDeel)
+        {
+            // SQL statement select
+            string sql = "Select * from Lid where GaatMee = @neemtDeel order by Naam ASC";
+
+            return (List<Lid>)db.Query<Lid>(sql, new { neemtDeel });
+        }
+
+        public void Inschrijven(Lid lid, bool neemtDeel)
+        {
+            // SQL Statement update
+            string sql = "Update Lid set GaatMee = @neemtDeel where id = @id";
+
+            // Uitvoeren SQL statement en doorgeven paramatercolletie
+            db.Execute(sql, new
+            {
+                neemtDeel,
+                lid.Id
+            });
+        }
+    }
+}
---
+++ Les 5 Dapper.net/Daguitstap - start/Daguitstap/ViewModel/LedenViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.ObjectModel;
+using Daguitstap.Model;
+using System.Windows.Input;
+
+namespace Daguitstap.ViewModel
+{
+    public class LedenViewModel : BaseViewModel
+    {
+        public LedenViewModel()
+        {
+            LeesGegevens();
+            KoppelenCommands();
+        }
+
+        // collection that allows code outside the collection be aware of when changes to the collection occur.
+        private ObservableCollection<Lid> leden;
+        public ObservableCollection<Lid> Leden
+        {
+            get { return leden; }
+            set {
+                leden = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Lid> deelnemers;
+        public ObservableCollection<Lid> Deelnemers
+        {
+            get { return deelnemers; }
+            set
+            {
+                deelnemers = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private Lid currentLid;
+        public Lid CurrentLid
+        {
+            get { return currentLid; }
+            set {
+                currentLid = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        // commands
+        private void KoppelenCommands()
+        {
+            InschrijvenCommand = new BaseCommand(Inschrijven);
+            UitschrijvenCommand = new BaseCommand(Uitschrijven);
+        }
+
+        public ICommand InschrijvenCommand { get; set; }
+        public ICommand UitschrijvenCommand { get; set; }
+
+        public void Inschrijven()
+        {
+            if (currentLid != null)
+            {
+                LidDataService lidDS = new LidDataService();
+                lidDS.Inschrijven(currentLid, true);
+
+                //Refresh
+                LeesGegevens();
+            }
+        }
+
+        public void Uitschrijven()
+        {
+            if (currentLid != null)
+            {
+                LidDataService lidDS = new LidDataService();
+                lidDS.Inschrijven(currentLid, false);
+
+                //Refresh
+                LeesGegevens();
+            }
+        }
+
+        private void LeesGegevens()
+        {
+            LidDataService lidDS = new LidDataService();
+
+            // een lid neemt niet per se deel
+            Leden = new ObservableCollection<Lid>(lidDS.GetDeelnemers(false));
+            Deelnemers = new ObservableCollection<Lid>(lidDS.GetDeelnemers(true));
+        }
+
+    }
+}
